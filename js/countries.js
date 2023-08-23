b97819d72180535fe61920f7f08b75dd9986c2c4
themarjanz <themarjanz@gmail.com>
2023-08-23 20:15:17 +0600

    third commit

--- js/countries.js
+++ js/countries.js
@@ -13,12 +13,37 @@ const displyCountries = countries => {
     container.innerHTML = countriesHTML.join('');
 }
 
-const getCountryHTML = country => {
+// const getCountryHTML = country => {
+//     return `
+//         <div class="country">
+//             <h2>${country.name.common}</h2>
+//             <img src="${country.flags.png}">
+//         </div>
+//     `
+// }
+
+// const getCountryHTML = country => {
+//     // option 1
+//     const { name, flags } = country;
+
+//     return `
+//         <div class="country">
+//             <h2>${name.common}</h2>
+//             <img src="${flags.png}">
+//         </div>
+//     `
+// }
+
+//option 2
+const getCountryHTML = ({ flags, name, area, region }) => {
     return `
         <div class="country">
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}">
+            <h2>${name.common}</h2>
+            <h3>${region}</h3>
+            <p>Area: ${area}<p/>
+            <img src="${flags.png}">
         </div>
     `
 }
+
 loadCountries();
